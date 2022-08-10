d577b9f50c1650d5e8240b94992f5dfa05d6df84
Rodz Amor <ramorSTEM2022@gmail.com>
2022-08-10 19:55:34 -0400

    Initial Commit

---
+++ TabView/TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewApp.swift
+//  TabView
+//
+//  Created by Andrie on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
